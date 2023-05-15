be71d84770aac02a20a6d59ad6631f0156b55555
Jeuneau <boekhoudtjeuneau@gmail.com>
2023-05-15 14:56:00 +0200

    initial commit

---
+++ Player.cs
@@ -0,0 +1,23 @@
+using Raylib_cs;
+using System; // Math
+using System.Numerics; //
+
+
+namespace platformer
+{
+   
+    class Player {
+
+        int g= 400;
+        float jumpSpeed= 350.0f;
+        float horSpeed= 200.0f;
+
+        Vector2 position;
+        float speed;
+        bool canJump;
+
+        //dddd
+
+    }
+
+}
